7c173a1359f0031b1a38be7d781784e512a6ea7a
Alena Tochilkina <alenatochilkina11@gmail.com>
2022-05-18 22:48:23 +0300

    Initial Commit

---
+++ MyMoneyDon'tJiggle/MyMoneyDon_tJiggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoneyDon_tJiggleApp.swift
+//  MyMoneyDon'tJiggle
+//
+//  Created by Alena Tochilkina on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMoneyDon_tJiggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
